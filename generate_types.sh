ac3a807688937887856a055f596da8a707b28132
23885 <kevin.boehme@aruplab.com>
2020-06-25 08:42:44 -0600

    Shoehorn a setup.py into the py project

--- generate_types.sh
+++ generate_types.sh
@@ -2,12 +2,12 @@
 set -x
 set -e
 
-quicktype ./objects/food.ts -o ./languages/py/food.py
+quicktype ./objects/food.ts -o ./languages/py/co/food.py
 quicktype ./objects/food.ts -o ./languages/go/food.go
 quicktype ./objects/food.ts -o ./languages/cs/food.cs
 quicktype ./objects/food.ts -o ./languages/java/food.java
 
-quicktype ./objects/bioinformatics.ts -o ./languages/py/bioinformatics.py
+quicktype ./objects/bioinformatics.ts -o ./languages/py/co/bioinformatics.py
 quicktype ./objects/bioinformatics.ts -o ./languages/go/bioinformatics.go
 quicktype ./objects/bioinformatics.ts -o ./languages/cs/bioinformatics.cs
 quicktype ./objects/bioinformatics.ts -o ./languages/java/bioinformatics.java
---
+++ languages/py/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+   name='co',
+   version='1.0',
+   description='Common Objects',
+   packages=['co'],
+)
